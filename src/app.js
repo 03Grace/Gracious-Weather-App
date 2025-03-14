85762dfb55cf5adf2767801c242bde7635060a6f
Grace Alilu <graceegbi1@gmail.com>
2025-03-14 21:08:32 +0100

    Added API integration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
+function refreshWeather(response) {
+  let tempratureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  tempratureElement.innerHTML = Math.round(temperature);
+}
+
+function cityTemp(city) {
+  let apiKey = "o031ff44025aa0btaa12d97420a0a3c6";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function searchCity(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#searched-city");
 
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  cityTemp(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
